aeaed4f61936b445c398d32b5b2147e46a1949b8
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2023-02-15 01:42:39 +0600

    add new task

--- lab3/ToDoApp/main.js
+++ lab3/ToDoApp/main.js
@@ -1,2 +1,30 @@
-const form = document.querySelector('#input');
-const inpTxt = document.querySelector('#inputText');
+const form = document.querySelector('.inpt');
+const inputTxt = document.querySelector('#inputText');
+const listOfTasks = document.querySelector('#listOfTasks');
+
+
+form.addEventListener('submit', addNewTask);
+
+
+
+function addNewTask(event) {
+  event.preventDefault();
+
+  const newText = inputTxt.value;
+
+  const addedBlock = `
+  <li>
+    <div class="one-item">
+      <input type="checkbox" class="checkbox">
+      <span class="added-text">${newText} </span>
+      <button type="button" class="trash" data-action="delete">
+        <image src="https://img.icons8.com/ios-glyphs/2x/filled-trash.png" width="20" ></image>
+      </button>
+    </div>
+  </li>`;
+
+  listOfTasks.insertAdjacentHTML(`beforeend`, addedBlock);
+
+  inputTxt.value = '';
+  inputTxt.focus();
+}
