0ea3c69cc58d8b7b957bdf1da82c7ce626924ed1
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2023-02-15 02:07:37 +0600

    remove task, done task

--- lab3/ToDoApp/main.js
+++ lab3/ToDoApp/main.js
@@ -5,7 +5,35 @@ const listOfTasks = document.querySelector('#listOfTasks');
 
 form.addEventListener('submit', addNewTask);
 
+listOfTasks.addEventListener('click', deleteTask);
 
+listOfTasks.addEventListener('click', doneTask);
+
+function doneTask(event) {
+  if (event.target.checked === true) {
+    // alert('k nwedkjn ')
+    const up = event.target.closest('li')
+    const addedText = up.querySelector('.added-text')
+    console.log(addedText)
+    addedText.classList.add('added-text-done')
+  }
+  else {
+    const up = event.target.closest('li')
+    const addedText = up.querySelector('.added-text')
+    console.log(addedText)
+    addedText.classList.remove('added-text-done')
+  }
+}
+
+
+function deleteTask(event) {
+  if (event.target.dataset.action == "delete") {
+    console.log('1234');
+    const up = event.target.closest('li');
+    // console.log(up);
+    up.remove();
+  }
+}
 
 function addNewTask(event) {
   event.preventDefault();
