5c6f7d490d69499c5ce2fac136c6078c9de9c558
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2023-02-20 15:28:38 +0600

    Kaspi products added

---
+++ lab4/kaspi/src/app/products.ts
@@ -0,0 +1,69 @@
+export interface Products{
+  image: string;
+  name: string;
+  description: string;
+  rating: number;
+}
+
+export const products = [
+  {
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h76/h2a/67021630144542/rakhat-shokoladnaya-plitka-molochnyi-100-g-100221859-1.jpg',
+    name: 'Kazakhstan chocolate',
+    description: 'chocolate milk 100 g',
+    rating: 4.8
+  },
+  {
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h8f/hd9/32515890741278/ulker-albeni-sokoladnyj-batoncik-molocnyj-pecene-40-g-100222662-1.jpg',
+    name: 'Albeni',
+    description: 'milk chocolate bar, cookies 40 g',
+    rating: 3.8
+  },
+  {
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hb2/h9d/50337844887582/oreo-kakao-i-vanilnyj-krem-228-g-100971349-1-Container.jpg',
+    name: 'Oreo',
+    description: 'Oreo cookies with cocoa and cream filling 228 g',
+    rating: 4.7
+  },
+  {
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h38/h88/62025154920478/belvita-utrennee-multi-zlakovoe-225-g-101000656-1-Container.jpg',
+    name: 'BelVita',
+    description: 'Morning oatmeal cookies without additives 225 g',
+    rating: 5
+  },
+  {
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/he5/h36/51302194970654/slastena-kukuruznye-palocki-200-g-100814351-1-Container.jpg',
+    name: 'Slastena',
+    description: 'Sweet corn sticks 200 g',
+    rating: 4.9
+  },
+  {
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/ha3/h00/46752118833182/nutella-orehovaa-s-dobavleniem-kakao-180-gr-100227056-1-Container.jpg',
+    name: 'Nutella',
+    description: 'NUTELLA hazelnut paste, cocoa 180 g',
+    rating: 5
+  },
+  {
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h41/h52/68744958050334/yashkino-vafel-nye-ruletiki-so-sgushchenkoi-160-g-100236474-1.jpg',
+    name: 'Yashkino Waffle rolls ',
+    description: 'Yashkino Waffle rolls with condensed milk 160 g',
+    rating: 4.6
+  },
+  {
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/ha8/ha1/46216838283294/sokolad-belyj-poristyj-alpen-gold-aerated-poristyj-80g-102442873-1.jpg',
+    name: 'Alpen Gold',
+    description: 'chocolate bar white 80 g',
+    rating: 5
+  },
+  {
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h88/h29/67249860411422/kinder-shokoladnaya-plitka-molochnyi-molochnaya-50-g-100221897-1.jpg',
+    name: 'Kinder',
+    description: 'KINDER milk chocolate bar, milk 50 g',
+    rating: 4.9
+  },
+  {
+    image: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h4a/hc3/50337229340702/ulker-biskrem-cocoa-180-g-100970648-1-Container.jpg',
+    name: 'Ulker Biskrem',
+    description: 'Ulker Biskrem Sosoa cookies with cocoa cream filling 180 g',
+    rating: 4.3
+  },
+];
