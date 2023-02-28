b9060b0f3a732bcecb5bdc3c9edb9080a9ad1abb
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2023-02-28 19:22:32 +0600

    product item component created

--- lab5/store/src/app/app.module.ts
+++ lab5/store/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ShippingComponent } from './shipping/shipping.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 @NgModule({
     imports: [
         BrowserModule,
@@ -30,7 +31,8 @@ import { ShippingComponent } from './shipping/shipping.component';
         ProductAlertsComponent,
         ProductDetailsComponent,
         CartComponent,
-        ShippingComponent
+        ShippingComponent,
+        ProductItemComponent
     ],
     exports: [
         TopBarComponent
