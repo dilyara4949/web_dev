403c8cb4d4c5df734f68f7fecb4b8b4a8e40da50
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2023-04-05 18:40:31 +0600

    get products

--- lab8/back_shop/api/urls.py
+++ lab8/back_shop/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, re_path
 from api import views
 
+
 urlpatterns = [
-    path('products/', views.product_list)
+    path('products/', views.product_list),
+    # path('products/<int:id>/', views.get_product),
+    # path('categories/', views.categories),
+    # path('categories/<int:id>/', views.category),
+    # path('products/<int:id>/products/', views.category_product),
 ]
--- lab8/back_shop/api/views.py
+++ lab8/back_shop/api/views.py
@@ -1,10 +1,16 @@
 # Create your views here.
 from django.http.response import JsonResponse
 
-from lab8.back_shop.api.models import Product
-
+from .models import Product
+#
 
 def product_list(request):
     products = Product.objects.all()
     products_json = [p.to_json() for p in products]
     return JsonResponse(products_json, safe=False)
+#
+#
+# def get_product(request, product_id):
+#     product = Product.objects.get(id=product_id)
+#     product_json = product.to_json()
+#     return JsonResponse(product_json, safe=False)
