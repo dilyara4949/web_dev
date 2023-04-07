58a667b224876e70fa4fa6870c8599a76c5026fb
dilyara4949 <90037377+dilyara4949@users.noreply.github.com>
2023-04-07 17:34:17 +0600

    list of companies done

--- lab9/hh_back/api/models.py
+++ lab9/hh_back/api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Company(models.Model):
     def __str__(self):
-        return f'(self.name)'
+        return f'{self.name}'
 
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -19,13 +19,13 @@ class Company(models.Model):
 
 class Vacancy (models.Model):
     def __str__(self):
-        return f'(self.name)'
+        return f'{self.name}'
 
 
     name = models.CharField(max_length=255)
     description = models.TextField()
     salary = models.FloatField()
-    category = models.ForeignKey(Company, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
 
     # def to_json(self):
--- lab9/hh_back/api/views.py
+++ lab9/hh_back/api/views.py
@@ -7,3 +7,19 @@ from .models import Company, Vacancy
 
 def home(request):
     return HttpResponse('home page')
+
+#
+
+def company_list(request):
+    return JsonResponse(list(Company.objects.values()), safe=False,json_dumps_params={'indent' : 2})
+
+
+
+#
+
+    # path('companies/', views.company_list),
+    # path('companies/<int:id>/', views.company),
+    # path('companies/<int:id>/vacancies/', views.company_vacancy),
+    # path('vacancies/', views.vacancy_list),
+    # path('vacancies/<int:id>/', views.vacancy),
+    # path('vacancies/top_ten/', views.vacancies_top),
